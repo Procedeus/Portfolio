741e56efc612d25ffd30a8d0c4340fac877c1abd
Murilo <murilorez79@hotmail.com>
2023-05-15 18:14:00 -0300

    Criação do Componente Header

---
+++ src/Components/Header/index.js
@@ -0,0 +1,29 @@
+import './index.css';
+import { SiMicrosoftoutlook } from 'react-icons/si';
+import { BsInstagram } from 'react-icons/bs';
+import { FaLinkedin } from 'react-icons/fa';
+
+const Header = () => {
+    return (
+        <div className="container">
+            <div className="col-4">
+                <div className='flex'>
+                    <h1>Olá, eu sou o Murilo</h1>
+                    <p>Formado em Análise e Desenvolvimento de Sistemas pela Faculdade de Tecnologia de Presidente Prudente.</p>
+                    <div>
+                        <SiMicrosoftoutlook className='icon'/>
+                        <BsInstagram className='icon'/>
+                        <FaLinkedin className='icon'/>
+                    </div>
+                    <div>
+                        <a href="/">Curriculo</a>
+                        <a href="/">Contatos</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+      
+
+export default Header;
