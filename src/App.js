d482daf0a8801da37eb8cb0247d331eb83918e34
Murilo <murilorez79@hotmail.com>
2023-05-16 15:47:14 -0300

    Criação do componente Conhecimentos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './Components/Header';
+import Conhecimentos from './Components/Conhecimentos';
 
 function App() {
   return (
     <>
       <Header/>
+      <Conhecimentos/>
     </>
   );
 }
