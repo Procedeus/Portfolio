28991f52c21ead94661161bac2ba454640e0661d
Murilo <murilorez79@hotmail.com>
2023-10-04 11:14:38 -0300

    Justify para os projetos

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -3,7 +3,7 @@ import './index.css';
 const Footer = () => {
     return (
         <footer>
-            <p>Criado por Murilo Rezende</p>
+            <p>Criado por Murilo Rezende &copy; 2023 - Todos os direitos reservados</p>
         </footer>
     );
 }
