702cee5d22c822a31b9773d5c36f7c0a00a09540
Murilo <murilorez79@hotmail.com>
2023-10-11 10:17:33 -0300

    style(projetos): Align items of the project-content

--- src/Components/Projetos/index.js
+++ src/Components/Projetos/index.js
@@ -17,15 +17,17 @@ const Projetos = () => {
                         <div className='project-text'>
                             <h2>Cursos de Instrumentos</h2>
                             <p>Uma landing page de cursos de instrumentos, apenas para praticar.</p>
+                        </div>
+                        <div>
                             <div className='project-icons'>
                                 <AiFillHtml5 /> 
                                 <PiFileCssFill /> 
                                 <LiaReact />
                             </div>
-                        </div>
-                        <div className='align-button'>
-                            <a href='https://github.com/Procedeus/curso-instrumentos' className='project-button color-blue'>Source Code</a>
-                            <a href='https://cursoinstrumentos.netlify.app/' className='project-button color-red'>Live Demo</a>
+                            <div className='align-button'>
+                                <a href='https://github.com/Procedeus/curso-instrumentos' className='project-button color-blue'>Source Code</a>
+                                <a href='https://cursoinstrumentos.netlify.app/' className='project-button color-red'>Live Demo</a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -36,8 +38,8 @@ const Projetos = () => {
                             <h2>Amigo Secreto</h2>
                             <p>Um projeto de amigo secreto, onde é criado uma tabela e adicionado as pessoas que irão participar, após isso é 
                                 sorteado e enviado um email para cada integrante.</p>
-                            <p>Tecnologias Utilizadas:</p>
-                            <p>Axios, EmailJs</p>
+                        </div>
+                        <div>
                             <div className='project-icons'>
                                 <AiFillHtml5 /> 
                                 <PiFileCssFill /> 
@@ -45,10 +47,10 @@ const Projetos = () => {
                                 <SiAxios />
                                 <MdEmail />
                             </div>
-                        </div>
-                        <div className='align-button'>
-                            <a href='https://github.com/Procedeus/React-amigo-secreto' className='project-button color-blue'>Source Code</a>
-                            <a href='https://react-amigo-secreto.netlify.app/login' className='project-button color-red'>Live Demo</a>
+                            <div className='align-button'>
+                                <a href='https://github.com/Procedeus/React-amigo-secreto' className='project-button color-blue'>Source Code</a>
+                                <a href='https://react-amigo-secreto.netlify.app/login' className='project-button color-red'>Live Demo</a>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -58,15 +60,17 @@ const Projetos = () => {
                         <div className='project-text'>
                             <h2>Jogo da Velha</h2>
                             <p>Um projeto de Jogo da Velha, ele permite que dois jogadores joguem o clássico jogo da velha em um ambiente web.</p>
+                        </div>
+                        <div>
                             <div className='project-icons'>
                                 <AiFillHtml5 /> 
                                 <PiFileCssFill /> 
                                 <LiaReact />
                             </div>
-                        </div>
-                        <div className='align-button'>
-                            <a href='https://github.com/Procedeus/jogo-da-velha' className='project-button color-blue'>Source Code</a>
-                            <a href='https://p-jogo-da-velha.netlify.app/' className='project-button color-red'>Live Demo</a>
+                            <div className='align-button'>
+                                <a href='https://github.com/Procedeus/jogo-da-velha' className='project-button color-blue'>Source Code</a>
+                                <a href='https://p-jogo-da-velha.netlify.app/' className='project-button color-red'>Live Demo</a>
+                            </div>
                         </div>
                     </div>
                 </div>
